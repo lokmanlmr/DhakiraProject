5d1d59e3b121316ce98e8b2da2f253734031e932
Lokos22 <130925977+lokmanlmr@users.noreply.github.com>
2024-05-11 00:51:32 +0100

    added a lot of stuff

--- app/src/main/java/com/example/dhakiraproject/Home.java
+++ app/src/main/java/com/example/dhakiraproject/Home.java
@@ -7,7 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.dhakiraproject.databinding.ActivityHomeBinding;
+import com.example.dhakiraproject.thefive.ARMainActivity;
+import com.example.dhakiraproject.thefive.Appointments;
+import com.example.dhakiraproject.thefive.Medications;
+import com.example.dhakiraproject.thefive.ProblemSolving;
 import com.example.dhakiraproject.thefive.ToDoActivity;
+import com.example.dhakiraproject.thefive.Tracking;
 
 public class Home extends AppCompatActivity {
     ActivityHomeBinding binding;
@@ -22,6 +27,45 @@ public class Home extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Home.this, ToDoActivity.class);
+                startActivity(intent);
+            }
+        });
+        binding.appointments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Appointments.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.medication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Medications.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.tracking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Tracking.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.ar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, ARMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.booster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, ProblemSolving.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/dhakiraproject/MainActivity.java
+++ app/src/main/java/com/example/dhakiraproject/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.transition.TransitionInflater;
 
 import com.example.dhakiraproject.databinding.ActivityMainBinding;
+import com.example.dhakiraproject.thefive.registeration.Register1;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -17,10 +20,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.letsGetStartedLayout.setOnClickListener(v -> {
-            /*
-            Intent intent = new Intent(MainActivity.this, Register.class);
+
+            Intent intent = new Intent(MainActivity.this, Register1.class);
             startActivity(intent);
-            */
+
 
         });
 
@@ -28,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
             //Go to Login Activity
             Intent intent = new Intent(MainActivity.this, Login.class);
             startActivity(intent);
+
         });
 
     }
+
+
 }
